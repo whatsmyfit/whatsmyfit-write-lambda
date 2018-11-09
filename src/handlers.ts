d05582a9945f3d23411b1f208c9cb62cb3fccc44
ismslo <ismar.slomic@vegvesen.no>
2018-11-09 08:57:28 +0100

    Made Notification API with swagger doc

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,19 +1,31 @@
 import { Callback, Context, Handler } from 'aws-lambda';
 
-interface IHelloResponse {
+interface INotificationResponse {
     statusCode: number;
-    body: string;
 }
 
-const hello: Handler = (event: any, context: Context, callback: Callback) => {
-    const response: IHelloResponse = {
-        body: JSON.stringify({
-            message: Math.floor(Math.random() * 2)
-        }),
-        statusCode: 200
+interface INotificationRequest {
+    notifications: INotification[];
+}
+
+interface INotification {
+    collectionType: string;
+    date: string;
+    ownerId: string;
+    ownerType: string;
+    subscriptionId: string;
+
+}
+
+const notification: Handler = (event: any, context: Context, callback: Callback) => {
+    const response: INotificationResponse = {
+        statusCode: 204
     };
+
+    const notificationArray = event.body as INotificationRequest;
+    console.log(notificationArray);
 
     callback(undefined, response);
 };
 
-export { hello };
+export { notification };
