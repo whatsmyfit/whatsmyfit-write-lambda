72cf50c2ed31c911787c88ef81ffa3c961084b79
ismslo <ismar.slomic@vegvesen.no>
2019-01-03 23:54:10 +0100

    Deploying stack to cicd env in aws for integration tests

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -1,5 +1,4 @@
 let config = require('./jest.config');
 config.testMatch = [ '**/__tests__/integration/**/*.itest.ts' ];
 console.log('*** RUNNING INTEGRATION TESTS ***');
-
 module.exports = config;
